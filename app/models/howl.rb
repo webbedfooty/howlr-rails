a716920f12b73a1a05baed2d20e5988dd26fc2ff
Davida Gaffney <gdavida@gmail.com>
2016-05-05 14:04:06 -0500

    Tests for Howls

--- app/models/howl.rb
+++ app/models/howl.rb
@@ -1,2 +1,6 @@
 class Howl < ActiveRecord::Base
+	validates :message, presence: true
+	validates :wolf_id, presence: true
+
+	belongs_to :wolves
 end
--- test/models/howl_test.rb
+++ test/models/howl_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class HowlTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "test is valid" do
+  	@howl = howls(:one)
+    assert(@howl.valid?, "This should be valid under normal circcumstances")
+  end
 end
