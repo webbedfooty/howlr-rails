f09089f106c9cf05b088265e30911e02f3389b54
Davida Gaffney <gdavida@gmail.com>
2016-05-05 20:07:00 -0500

    wolf index view and index work

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,18 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Howl.create(message: "Hooooooooowwwwwl", howl_image: "https://s3-us-west-2.amazonaws.com/wolfimages/share-1.jpg", wolf_id: 1)
-
 Wolf.create(name: "Aaron Carter", profile_image: "https://s3-us-west-2.amazonaws.com/wolfimages/wolf-1.jpg", bio: "Now walking on the front streets")
+Wolf.create!(name:"John", profile_image:"https://s3-us-west-2.amazonaws.com/wolfimages/wolf-1.jpg", bio:"RAAAARRRRRR")
+Wolf.create!(name:"Paul", profile_image:"https://s3-us-west-2.amazonaws.com/wolfimages/wolf-2.jpg", bio:"Howdy pals.")
+Wolf.create!(name:"George", profile_image:"https://s3-us-west-2.amazonaws.com/wolfimages/wolf-3.jpg", bio:"Beep Boop Bop Beep Boop Boop")
+Wolf.create!(name:"Ringo", profile_image:"https://s3-us-west-2.amazonaws.com/wolfimages/wolf-4.jpg", bio:"Ahhhhooooooooo")
+Wolf.create!(name:"Delores", profile_image:"https://s3-us-west-2.amazonaws.com/wolfimages/wolf-5.jpg", bio:"Here's a bio about me...")
+
+Howl.create!(message:"Aroooooooooo!!", wolf_id: 1, howl_image: "https://s3-us-west-2.amazonaws.com/wolfimages/share-1.jpg")
+Howl.create!(message:"Pretty cool project. As far as things go.", wolf_id: 1)
+Howl.create!(message:"I've included Bootstrap for you, but you don't need to use Bootstrap.", wolf_id: 2)
+Howl.create!(message:'We should be able to "like" a message.', wolf_id: 4, howl_image: "https://s3-us-west-2.amazonaws.com/wolfimages/share-3.jpg")
+Howl.create!(message:"Hint: I'm the wolf.", wolf_id: 4, howl_image: "https://s3-us-west-2.amazonaws.com/wolfimages/share-4.jpg")
+Howl.create!(message:"Think about what happens for each page if there are no wolves in the database.", wolf_id: 4)
+Howl.create!(message:"Don't try to be a hero.", wolf_id: 4)
+Howl.create(message: "Hooooooooowwwwwl", howl_image: "https://s3-us-west-2.amazonaws.com/wolfimages/share-1.jpg")
